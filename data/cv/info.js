6a5edf9ee3d29859b4584b5827fe61268a98c1a4
Serhii Mamedov <43890857+Repugraf@users.noreply.github.com>
2020-12-15 18:24:45 +0200

    Update info.js

--- data/cv/info.js
+++ data/cv/info.js
@@ -1,4 +1,4 @@
-export default `<p>Current residence: Lviv</p>
+export default `<p>Current residence: Lviv, Ukraine</p>
 <p>Tel: (+380) 099 50 70 544</p>
 <p>Email: <a href="mailto:Bboyvivus@gmail.com">Bboyvivus@gmail.com</a></p>
 <p>FB: <a href="https://www.facebook.com/Repugraf">https://www.facebook.com/Repugraf</a></p>
