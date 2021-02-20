c0372a5dc1b480f0879de1545f70d67cf46d0d0d
Serhii Mamedov <bboyvivus@gmail.com>
2021-02-20 12:34:17 +0200

    Update summary

--- data/cv/summary.js
+++ data/cv/summary.js
@@ -1,6 +1,16 @@
-export default `<p>I'm a Full-Stack web developer.</p>
-<p>Experienced working front-end, backend and deployment of web apps. That’s why I like doing all of it.</p>
-<p>Pretty friendly with people so working with a team is easy for me.</p>
-<p>Always improve my skills by learning new stuff.</p>
-<p>Most important thing in work is clear requirements for the task/project.</p>
-<p>My favorite tool for development is Visual Studio Code.</p>`;
+export default `
+  <p>Experienced working front-end, backend and deployment of web apps.</p>
+  <p>Mostly experienced with <strong>JavaScript</strong> but not limited to it.</p>
+  <p>Here are some of the industries I've made software for:</p>
+  <ul>
+    <li>Accounting, Audit</li>
+    <li>Mortgage</li>
+    <li>Telecommunications</li>
+    <li>BI, Data Services</li>
+    <li>SaaS</li>
+  </ul>
+  <p>
+    Worked for plenty of clients/companies all over the world
+    (UK, Israel, Canada, New Zealand, Poland).
+  </p>
+`
